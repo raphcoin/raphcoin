50ef472d529e07f83722a0f957e25839f480a669
David Ackerman <enfipy@gmail.com>
2019-05-30 18:57:34 +0300

    Add basic logic

--- runtime/src/template.rs
+++ runtime/src/template.rs
@@ -1,123 +1,114 @@
-/// A runtime module template with necessary imports
-
-/// Feel free to remove or edit this file as needed.
-/// If you change the name of this file, make sure to update its references in runtime/src/lib.rs
-/// If you remove this file, you can remove those references
-
-
-/// For more guidance on Substrate modules, see the example module
-/// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
-
-use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
+use parity_codec::{Decode, Encode};
+use runtime_primitives::traits::As;
+use support::{
+	decl_event, decl_module, decl_storage,
+	dispatch::Result, ensure, StorageMap,
+};
 use system::ensure_signed;
 
-/// The module's configuration trait.
-pub trait Trait: system::Trait {
-	// TODO: Add other types and constants required configure this module.
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Resource<Balance> {
+	level: u32,
+	time: u64,
+	accrual: Balance,
+	rate: u32,
+	resource_amount: Balance,
+}
 
-	/// The overarching event type.
+pub trait Trait: balances::Trait + timestamp::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
-/// This module's storage items.
+decl_event!(
+	pub enum Event<T>
+	where
+		AccountId = <T as system::Trait>::AccountId,
+	{
+		PlayerCreated(AccountId),
+	}
+);
+
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item. 
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(something): Option<u32>;
+		GoldenMines get(golden_mine): map T::AccountId => Resource<T::Balance>;
 	}
 }
 
 decl_module! {
-	/// The module declaration.
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Initializing events
-		// this is needed only if you are using events in your module
 		fn deposit_event<T>() = default;
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
+		pub fn init(origin) -> Result {
 			let who = ensure_signed(origin)?;
+			ensure!(!<GoldenMines<T>>::exists(who.clone()), "Player already has account");
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			<Something<T>>::put(something);
+			// let block_number = <system::Module<T>>::block_number();
+			// let minimum_period = <timestamp::Module<T>>::minimum_period();
+			// block_number.as_() * minimum_period.as_();
+			let now: u64 = <timestamp::Module<T>>::now().as_();
 
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			let res = Resource {
+				level: 0,
+				time: now,
+				accrual: <T::Balance as As<u64>>::sa(0),
+				rate: 0,
+				resource_amount: <T::Balance as As<u64>>::sa(100),
+			};
+			<GoldenMines<T>>::insert(who.clone(), res);
+
+			Self::deposit_event(RawEvent::PlayerCreated(who));
 			Ok(())
 		}
-	}
-}
 
-decl_event!(
-	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		// Just a dummy event.
-		// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		// To emit this event, we call the deposit funtion, from our runtime funtions
-		SomethingStored(u32, AccountId),
-	}
-);
+		pub fn buy_golden_mine(origin) -> Result {
+			let who = ensure_signed(origin)?;
+			ensure!(<GoldenMines<T>>::exists(who.clone()), "You don't have this resource");
 
-/// tests for this module
-#[cfg(test)]
-mod tests {
-	use super::*;
+			let mut res = Self::golden_mine(who.clone());
+			ensure!(res.level == 0, "Resource already bought");
 
-	use runtime_io::with_externalities;
-	use primitives::{H256, Blake2Hasher};
-	use support::{impl_outer_origin, assert_ok};
-	use runtime_primitives::{
-		BuildStorage,
-		traits::{BlakeTwo256, IdentityLookup},
-		testing::{Digest, DigestItem, Header}
-	};
+			let gold_mine_price = <T::Balance as As<u64>>::sa(100);
+			ensure!(res.resource_amount >= gold_mine_price, "You don't have enough gold");
 
-	impl_outer_origin! {
-		pub enum Origin for Test {}
-	}
+			let now: u64 = <timestamp::Module<T>>::now().as_();
 
-	// For testing the module, we construct most of a mock runtime. This means
-	// first constructing a configuration type (`Test`) which `impl`s each of the
-	// configuration traits of modules we want to use.
-	#[derive(Clone, Eq, PartialEq)]
-	pub struct Test;
-	impl system::Trait for Test {
-		type Origin = Origin;
-		type Index = u64;
-		type BlockNumber = u64;
-		type Hash = H256;
-		type Hashing = BlakeTwo256;
-		type Digest = Digest;
-		type AccountId = u64;
-		type Lookup = IdentityLookup<Self::AccountId>;
-		type Header = Header;
-		type Event = ();
-		type Log = DigestItem;
-	}
-	impl Trait for Test {
-		type Event = ();
-	}
-	type TemplateModule = Module<Test>;
+			res.level = 1;
+			res.time = now;
+			res.accrual = <T::Balance as As<u64>>::sa(1);
+			res.rate = 1;
+			res.resource_amount -= gold_mine_price;
 
-	// This function basically just builds a genesis storage key/value store according to
-	// our desired mockup.
-	fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
-		system::GenesisConfig::<Test>::default().build_storage().unwrap().0.into()
-	}
+			<GoldenMines<T>>::insert(who.clone(), res);
 
-	#[test]
-	fn it_works_for_default_value() {
-		with_externalities(&mut new_test_ext(), || {
-			// Just a dummy test for the dummy funtion `do_something`
-			// calling the `do_something` function with a value 42
-			assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-			// asserting that the stored value is equal to what we stored
-			assert_eq!(TemplateModule::something(), Some(42));
-		});
+			Ok(())
+		}
+
+		pub fn level_up(origin) -> Result {
+			let who = ensure_signed(origin)?;
+			ensure!(<GoldenMines<T>>::exists(who.clone()), "You don't have this resource");
+
+			let mut res = Self::golden_mine(who.clone());
+			ensure!(res.level != 0, "You have to buy this resource first");
+
+			let now: u64 = <timestamp::Module<T>>::now().as_();
+
+			let next_level_price = <T::Balance as As<u64>>::sa((res.level * res.rate * 100u32) as u64) * res.accrual;
+
+			// Todo: Move to private func
+			let tmp = now - res.time;
+			let resource_amount = <T::Balance as As<u64>>::sa(tmp / (res.rate as u64)) * res.accrual;
+
+			ensure!(resource_amount >= next_level_price, "You don't have enough gold");
+
+			res.level += 1;
+			res.time = now;
+			res.accrual += <T::Balance as As<u64>>::sa(1);
+			res.resource_amount = resource_amount - next_level_price;
+
+			<GoldenMines<T>>::insert(who.clone(), res);
+
+			Ok(())
+		}
 	}
 }
